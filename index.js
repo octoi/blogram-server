c91104139d0a25c93084f697159f665074df73f5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-23 12:42:17 +0530

    using routes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require("cors");
 
+const userRoute = require("./routes/userRoute");
+const blogRoute = require('./routes/blogRoute');
+
 const app = express();
 
+// MIDDLEWARE
 app.use(cors()); // enabling cors 
 
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
+
+// ROUTE
 app.get('/', (req, res) => res.status(200).send('Server is up 😎'));
 
-// server listening
+// LISTENER
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`[+] SERVER STARTED ON PORT ${port}`);
