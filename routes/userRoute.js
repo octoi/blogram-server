5523b6c5d06163e414d97e528f282d21c1d2283d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-23 12:36:01 +0530

    basic route setup

---
+++ routes/userRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express();
+
+
+router.post('/register', (req, res) => {
+	// register user
+});
+
+router.post('/login', (req, res) => {
+	// login user
+});
+
+router.put('/update', (req, res) => {
+	// update user
+});
