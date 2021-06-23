5ebb5f0103923462be23e43ec67f21f6ec74b571
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-23 13:34:34 +0530

    login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../mongo/userHelper');
+const { register, login } = require('../mongo/userHelper');
 const { generateSuccessMessage } = require('../utils/generator');
 
 router.post('/register', (req, res) => {
 
-	register(req.body).then(user => {
-
-		res.json(generateSuccessMessage(user));
-
-	}).catch(err => {
-
-		res.json(err)
-
-	})
+	register(req.body)
+		.then(user => res.json(generateSuccessMessage(user)))
+		.catch(err => res.json(err))
 
 });
 
 router.post('/login', (req, res) => {
-	// login user
+
+	login(req.body)
+		.then(user => res.json(generateSuccessMessage(user)))
+		.catch(err => res.json(err))
+
 });
 
 router.put('/update', (req, res) => {
