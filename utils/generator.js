d9057527a0fa7187e2a5095879d43c475388ffba
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-23 13:15:21 +0530

    generate jwt token

--- utils/generator.js
+++ utils/generator.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 const generateErrorMessage = message => {
 	return {
 		status: 404,
@@ -5,5 +8,12 @@ const generateErrorMessage = message => {
 	}
 }
 
+const generateToken = user => {
+	return jwt.sign({
+		id: user.id,
+		username: user.username,
+		name: user.name,
+	}, process.env.JWT_KEY, { expiresIn: '1h' });
+}
 
-module.exports = { generateErrorMessage }
+module.exports = { generateErrorMessage, generateToken }
