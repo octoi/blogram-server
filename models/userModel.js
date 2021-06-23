371554a1139e71009384d458a76ad22ac2d058c2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-23 13:02:11 +0530

    user model

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require('mongoose');
+
+const UserSchema = new Schema({
+	name: String,
+	email: String,
+	username: String,
+	password: String
+});
+
+const userModel = model('User', UserSchema);
+
+module.exports = userModel
