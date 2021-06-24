72aec7e6f357110c7e802d3fa2161cc73018cf89
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-24 12:33:04 +0530

    new blog sample func

--- mongo/blogHelper.js
+++ mongo/blogHelper.js
@@ -1,5 +1,6 @@
 const Blog = require('../models/blogModel');
 const { generateErrorMessage } = require('../utils/generator');
+const { login } = require('./userHelper');
 
 module.exports = {
 
@@ -32,6 +33,46 @@ module.exports = {
 
 				console.log(`[-] FAILED TO FETCH BLOG WITH ID ${id}`);
 				reject(generateErrorMessage('Failed to fetch blog !'));
+
+			}
+		});
+	},
+
+	create: ({ title, blog, username, password }) => {
+		return new Promise((resolve, reject) => {
+			try {
+
+				login(username, password)
+					.catch((err) => reject(generateErrorMessage(err.message)))
+					.then(async (userData) => {
+
+						if (!userData) return;
+
+						const newBlog = new Blog({
+							title,
+							blog,
+							createdAt: Date.now(),
+							user: userData?.id,
+						});
+
+						const blogRes = await newBlog.save();
+
+						const data = {
+							title: blogRes.title,
+							blog: blogRes.description,
+							id: blogRes._id,
+							createdAt: blogRes.createdAt,
+							user: blogRes.user,
+						}
+
+						resolve(data);
+
+					});
+
+			} catch (err) {
+
+				console.log('[-] FAILED TO CREATE NEW BLOG');
+				reject(generateErrorMessage('Failed to create blog !'));
 
 			}
 		});
